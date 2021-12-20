d8585ef4780d6366c7e0d22214929b815fcca478
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-12-21 02:05:09 +0900

    #2.2 preload font, image, asset

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import AppLoading from "expo-app-loading";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./pepe.jpeg"));
+    await Image.prefetch(
+      "https://images.chosun.com/resizer/u9nJRxs0BbtjygJ4HzKukecXnOk=/464x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/UVBJZL3RXAB36BDSHVM3MW2WNY.jpg"
+    );
   };
 
   if (!ready) {
