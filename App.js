268869a4738b4b89de7eac194eabbad7147d6f92
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-12-21 02:13:43 +0900

    #2.3.1 Make function for loading fonts, assets, images

--- App.js
+++ App.js
@@ -5,15 +5,20 @@ import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from "expo-asset";
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadAssets = (assets) => assets.map((asset) => Asset.loadAsync(asset));
+const loadImages = (images) => images.map((image) => Image.prefetch(image));
+
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./pepe.jpeg"));
-    await Image.prefetch(
-      "https://images.chosun.com/resizer/u9nJRxs0BbtjygJ4HzKukecXnOk=/464x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/UVBJZL3RXAB36BDSHVM3MW2WNY.jpg"
-    );
+    const fonts = loadFonts([Ionicons.font]);
+    const assets = loadAssets([require("./pepe.jpeg")]);
+    const images = loadImages([
+      "https://images.chosun.com/resizer/u9nJRxs0BbtjygJ4HzKukecXnOk=/464x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/UVBJZL3RXAB36BDSHVM3MW2WNY.jpg",
+    ]);
+    await Promise.all([...fonts, ...assets, ...images]);
   };
 
   if (!ready) {
